773dabbd0b0039036e4a424225be04b609940a3b
Freeads1234 <a33898130@gmail.com>
2025-01-18 23:23:49 +0530

    image upload error fix

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie';
 
 const instance = axios.create({
   baseURL: 'http://13.201.137.144/',
-  // baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'http://127.0.0.1:8000',
 
 });
 
