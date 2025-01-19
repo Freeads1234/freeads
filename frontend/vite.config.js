55d25df1bbd3dffcb9617894b9a0a5ba3d3577e4
Freeads1234 <a33898130@gmail.com>
2025-01-19 17:07:44 +0530

    media

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,16 +7,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/static': {
-        target: `${BACKEND_URL}`, 
-        changeOrigin: true,
-        secure: false,
-      },
-      '/media': {
-        target: `${BACKEND_URL}`, 
-        changeOrigin: true,
-        secure: false,
-      },
+      // '/static': {
+      //   target: `${BACKEND_URL}`, 
+      //   changeOrigin: true,
+      //   secure: false,
+      // },
+      // '/media': {
+      //   target: `${BACKEND_URL}`, 
+      //   changeOrigin: true,
+      //   secure: false,
+      // },
       '/admin': {
         target: `${BACKEND_URL}`,
         changeOrigin: true,
