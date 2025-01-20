7ecfbcdd7e470d9d8dda7c1e092df8b07ad2e3df
Freeads1234 <a33898130@gmail.com>
2025-01-20 22:27:57 +0530

    backend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-export const BACKEND_URL = "http://43.204.219.82";
+// export const BACKEND_URL = "http://43.204.219.82";
 // export const BACKEND_URL = "http://127.0.0.1:8000";
-// export const BACKEND_URL = "http://backend";
+export const BACKEND_URL = "http://backend";
 
 
