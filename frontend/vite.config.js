6ac01f9b342559a2020b0520029cb684e9b2e6f6
Freeads1234 <a33898130@gmail.com>
2025-01-21 00:44:51 +0530

    media

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,28 +5,4 @@ import { BACKEND_URL } from './src/config';
 // vite.config.js
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // proxy: {
-      // '/static': {
-      //   target: ${BACKEND_URL}, 
-      //   changeOrigin: true,
-      //   secure: false,
-      // },
-      // '/media': {
-      //   target: ${BACKEND_URL}, 
-      //   changeOrigin: true,
-      //   secure: false,
-      // },
-      // '/admin': {
-      //   target: BACKEND_URL,
-      //   changeOrigin: true,
-      //   secure: true,
-      // },
-      // '/api': {
-      //   target: BACKEND_URL,
-      //   changeOrigin: true,
-      //   secure: true,
-      // },
-    // },
-  },
 });
